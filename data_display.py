ebe2318f47f605242132ade376e65b581a660c06
unknown <xiongthomas@foxmail.com>
2023-05-23 12:46:07 +0800

    update data

--- data_display.py
+++ data_display.py
@@ -298,7 +298,7 @@ def select_and_show_hetero_data():
     detail_light_sets = detail_light_sets.set_index('time')
 
     hetero = pd.concat((detail_light_sets,test_hetero),axis=1)
-    hetero = hetero.fillna(0)
-    st.write(hetero)
+    #hetero = hetero.fillna(0)
+    #st.write(hetero)
 
     show_everything(hetero)
